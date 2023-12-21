7b8a6e73847020355dbed35274e98bd4b8acc7fc
Harm Meiejr <harm.meijer@commercetools.com>
2023-12-21 13:52:13 +0100

    initial commit

---
+++ mc-app/src/components/channels/channels.tsx
@@ -0,0 +1,54 @@
+import {
+  useMcMutation,
+  useMcQuery,
+} from '@commercetools-frontend/application-shell-connectors';
+import UpdateCustomObjectMutation from '../../hooks/use-channels-connector/update-custom-object.ctp.graphql';
+import FetchCustomObjectQuery from '../../hooks/use-channels-connector/fetch-custom-object.ctp.graphql';
+import { GRAPHQL_TARGETS } from '@commercetools-frontend/constants';
+import { useCallback } from 'react';
+
+const Channels = () => {
+  const { data, loading, refetch } = useMcQuery(FetchCustomObjectQuery, {
+    variables: {
+      key: 'certification-comments-af6c6cf0-f19b-4101-b761-b6f139a2b0e0',
+      container: 'certification-comments',
+    },
+    context: {
+      target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM,
+    },
+  });
+  const [updateCustomObject, { loading: updating }] = useMcMutation(
+    UpdateCustomObjectMutation
+  );
+  //@ts-ignore
+  const version = data?.customObject?.version;
+  const update = useCallback(() => {
+    updateCustomObject({
+      context: {
+        target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM,
+      },
+      variables: {
+        version,
+        key: 'certification-comments-af6c6cf0-f19b-4101-b761-b6f139a2b0e0',
+        container: 'certification-comments',
+        value: JSON.stringify({ field: `Random: ${Math.random()}` }),
+      },
+    }).then(() => {
+      refetch();
+    });
+  }, [refetch, updateCustomObject, version]);
+  console.log('loading is not set correctly again', loading);
+  return (
+    <div>
+      {!loading && <pre>{JSON.stringify(data, undefined, 2)}</pre>}
+      {!loading && (
+        <button onClick={update} disabled={updating}>
+          update
+        </button>
+      )}
+    </div>
+  );
+};
+Channels.displayName = 'Channels';
+
+export default Channels;
