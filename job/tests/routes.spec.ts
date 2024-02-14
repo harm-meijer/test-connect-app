5893c60908a5241b5321f7046298110585ac2c38
Harm Meiejr <harm.meijer@commercetools.com>
2024-02-14 13:28:46 +0100

    fail test if not world

--- job/tests/routes.spec.ts
+++ job/tests/routes.spec.ts
@@ -1,7 +1,11 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { describe, expect, test } from '@jest/globals';
 
 describe('Testing Job', () => {
   test('Mock Job testing', async () => {
+    expect(process.env.HELLO).toBe('world');
     expect(1).toBe(1);
   });
 });
