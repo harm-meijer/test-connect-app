051a34a311272b4944b5a252b818014c119eaedd
Harm Meiejr <harm.meijer@commercetools.com>
2024-02-16 09:53:57 +0100

    hello world service

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,20 +1,5 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 import express, { Express } from 'express';
-import bodyParser from 'body-parser';
-
-// Import routes
-import ServiceRoutes from './routes/service.route';
-
-// Import logger
 import { logger } from './utils/logger.utils';
-
-import { readConfiguration } from './utils/config.utils';
-import { errorMiddleware } from './middleware/error.middleware';
-
-// Read env variables
-readConfiguration();
 
 const PORT = 8080;
 
@@ -23,14 +8,11 @@ const app: Express = express();
 app.disable('x-powered-by');
 
 // Define configurations
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define routes
-app.use('/service', ServiceRoutes);
-
-// Global error handler
-app.use(errorMiddleware);
+app.use('/service', (req, res) => {
+  res.json({ message: 'Service is running' });
+});
 
 // Listen the application
 const server = app.listen(PORT, () => {
