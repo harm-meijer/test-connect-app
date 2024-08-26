d81b3f01fa97c7a222a019a174dbcd39dc8b572a
Harm Meijer <harm.meijer@commercetools.com>
2024-08-26 11:18:23 +0200

    with inheritAs shared config

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,44 +1,30 @@
-import express, { Express, Request } from 'express';
+import * as express from 'express';
+import { Express } from 'express';
 import { logger } from './utils/logger.utils';
-import {
-  createSessionMiddleware,
-  CLOUD_IDENTIFIERS,
-} from '@commercetools-backend/express';
 import { config } from 'dotenv';
 config();
 
 const PORT = 8080;
 
 // Create the express app
-const app: Express = express();
-app.use(
-  createSessionMiddleware({
-    audience: process.env.SERVICE_URL as string,
-    issuer: CLOUD_IDENTIFIERS.GCP_EU,
-  })
-);
+const app: Express = (
+  express as unknown as { default: () => Express }
+).default();
 app.disable('x-powered-by');
 
 // Define configurations
 
 // Define routes
-type TSession = {
-  session: {
-    userId: string;
-    projectKey: string;
-    userPermissions?: string[];
-  };
-} & Request;
-app.use('/service', (req, res) => {
-  const r = req as TSession;
+app.use('/service', (_req, res) => {
   res.json({
-    message: 'Service is running',
-    userId: r.session.userId,
-    projectKey: r.session.projectKey,
-    userPermissions: r.session.userPermissions,
+    SERVICE_URL: process.env.SERVICE_URL,
+    GLOBAL_SECURED: process.env.GLOBAL_SECURED,
+    GLOBAL_STANDARD: process.env.GLOBAL_STANDARD,
   });
 });
 logger.info(`service url ${process.env.SERVICE_URL}`);
+logger.info(`global seured ${process.env.GLOBAL_SECURED}`);
+logger.info(`global standard ${process.env.GLOBAL_STANDARD}`);
 // Listen the application
 const server = app.listen(PORT, () => {
   logger.info(`⚡️ Service application listening on port ${PORT}`);
